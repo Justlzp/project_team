d14e31a3cb44c50aca43cd009d310517599637c6
guohongming <1006575211@qq.com>
2017-09-17 16:09:43 +0800

    test
    测试一下提交权限

--- webDemo/manage.py
+++ webDemo/manage.py
@@ -8,7 +8,7 @@ app = create_app('movieapp.config.DevConfig')
 manager = Manager(app)
 manager.add_command("runserver", Server())
 
-
+#shell
 @manager.shell
 def make_shell_context():
     return dict(app=app, db=models.db_user, User=models.User)
